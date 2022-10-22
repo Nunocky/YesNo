ad5a9d8c82dd20fdd0607e3127451c553c6a7d28
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2022-10-22 09:30:29 +0900

    ファイル分割

--- app/src/main/java/com/example/yesno/MainFragment.kt
+++ app/src/main/java/com/example/yesno/MainFragment.kt
@@ -4,66 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.Glide
-import com.example.yesno.api.YesNoRepository
-import com.example.yesno.data.YesNo
 import com.example.yesno.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor(
-    private val repository: YesNoRepository
-) : ViewModel() {
-    private val _fetchState = MutableStateFlow<FetchState>(FetchState.Init)
-    val fetchState: StateFlow<FetchState> = _fetchState
-
-    val isFetching = fetchState.map { it is FetchState.Fetching }.asLiveData()
-
-    val text = fetchState.map { state ->
-        when (state) {
-            FetchState.Init -> ""
-            FetchState.Fetching -> "Fetching..."
-            is FetchState.Success -> state.yesno.answer
-            is FetchState.Fail -> "Failed"
-        }
-    }.asLiveData()
-
-    fun processFetch() = viewModelScope.launch(Dispatchers.IO) {
-
-        _fetchState.value = FetchState.Fetching
-
-        val response = repository.fetch()
-
-        response
-            .onSuccess {
-                _fetchState.value = FetchState.Success(it)
-            }.onFailure {
-                _fetchState.value = FetchState.Fail(it)
-            }
-    }
-
-    sealed class FetchState {
-        object Init : FetchState()
-        object Fetching : FetchState()
-        class Success(val yesno: YesNo) : FetchState()
-        class Fail(val th: Throwable) : FetchState()
-    }
-
-}
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -101,11 +47,7 @@ class MainFragment : Fragment() {
                     }
 
                     is MainViewModel.FetchState.Fail -> {
-                        Toast.makeText(
-                            requireActivity(),
-                            "Failed(${state.th.message})",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        binding.imageView.setImageBitmap(null)
                     }
                 }
             }
---
+++ app/src/main/java/com/example/yesno/MainViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.yesno
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.yesno.api.YesNoRepository
+import com.example.yesno.data.YesNo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: YesNoRepository
+) : ViewModel() {
+    sealed class FetchState {
+        object Init : FetchState()
+        object Fetching : FetchState()
+        class Success(val yesno: YesNo) : FetchState()
+        class Fail(val th: Throwable) : FetchState()
+    }
+
+    private val _fetchState = MutableStateFlow<FetchState>(FetchState.Init)
+    val fetchState: StateFlow<FetchState> = _fetchState
+
+    val isFetching = fetchState.map { it is FetchState.Fetching }.asLiveData()
+
+    val text = fetchState.map { state ->
+        when (state) {
+            FetchState.Init -> ""
+            FetchState.Fetching -> "Fetching..."
+            is FetchState.Success -> state.yesno.answer
+            is FetchState.Fail -> "Failed"
+        }
+    }.asLiveData()
+
+    fun processFetch() = viewModelScope.launch(Dispatchers.IO) {
+
+        _fetchState.value = FetchState.Fetching
+
+        val response = repository.fetch()
+
+        response
+            .onSuccess {
+                _fetchState.value = FetchState.Success(it)
+            }.onFailure {
+                _fetchState.value = FetchState.Fail(it)
+            }
+    }
+}
