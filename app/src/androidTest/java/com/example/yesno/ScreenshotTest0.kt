76898dc47fb6b7171b8aed71d9503ba782239d6b
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2022-11-07 19:28:16 +0900

    spoon対応中
        Unable to create output dir: /storage/emulated/0/app_spoon-screenshots
    
    が出る。これだけ直せればいいはず

---
+++ app/src/androidTest/java/com/example/yesno/ScreenshotTest0.kt
@@ -0,0 +1,46 @@
+package com.example.yesno
+
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
+import com.squareup.spoon.SpoonRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+@LargeTest
+@HiltAndroidTest
+class ScreenshotTest0 {
+    @get:Rule(order = 1)
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 2)
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule(order = 3)
+    var runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        READ_EXTERNAL_STORAGE,
+        WRITE_EXTERNAL_STORAGE
+    )
+
+    @Rule(order = 4)
+    @JvmField
+    val spoon = SpoonRule()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun test0() {
+        activityScenarioRule.scenario.onActivity {
+            val file = spoon.screenshot(it, "xxx")
+        }
+    }
+}
